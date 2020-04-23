4a69b9eec78b4c28c64c461630f1b961745308f3
Mickaël Malécot <mickael.malecot@gmail.com>
2020-04-23 13:04:42 +0200

    Improve mouse input example

--- examples/mouse_input.rs
+++ examples/mouse_input.rs
@@ -2,6 +2,11 @@ use rayquaza::core::{
     color::Color, input::MouseButton, math::Vector2, result::Result, window::WindowBuilder,
 };
 
+struct Ball {
+    position: Vector2,
+    color: Color,
+}
+
 fn main() -> Result {
     let window = WindowBuilder::new()
         .size(800, 450)
@@ -9,23 +14,26 @@ fn main() -> Result {
         .vsync()
         .msaa_4x()
         .build()?;
-    let mut ball = (Vector2::default(), Color::DARKBLUE);
+    let mut ball = Ball {
+        position: Vector2::default(),
+        color: Color::DARKBLUE,
+    };
     while !window.should_close() {
         // Update
-        ball.0 = window.get_mouse_position();
-        ball.1 = if window.is_mouse_button_pressed(MouseButton::Left) {
+        ball.position = window.get_mouse_position();
+        ball.color = if window.is_mouse_button_pressed(MouseButton::Left) {
             Color::MAROON
         } else if window.is_mouse_button_pressed(MouseButton::Right) {
             Color::DARKBLUE
         } else if window.is_mouse_button_pressed(MouseButton::Middle) {
             Color::LIME
         } else {
-            ball.1
+            ball.color
         };
         // Draw
         window.draw(|canvas| {
             canvas.clear_background(Color::RAYWHITE);
-            canvas.draw_circle_vec(ball.0, 40.0, ball.1);
+            canvas.draw_circle_vec(ball.position, 40.0, ball.color);
             canvas.draw_text(
                 "Move ball with mouse and click mouse button to change color",
                 10,
