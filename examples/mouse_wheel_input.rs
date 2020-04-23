5bb7001e0cd3e0fc5c1dc944ed24d2fec0725387
Mickaël Malécot <mickael.malecot@gmail.com>
2020-04-23 13:06:00 +0200

    Improve mouse wheel input example

--- examples/mouse_wheel_input.rs
+++ examples/mouse_wheel_input.rs
@@ -9,24 +9,18 @@ fn main() -> Result {
         .title("Mouse wheel input")
         .vsync()
         .build()?;
-    let mut box_position = (
+    let mut position = (
         window.get_screen_width() / 2 - BOX_SIZE / 2,
         window.get_screen_height() / 2 - BOX_SIZE / 2,
     );
     while !window.should_close() {
         // Update
         let delta = window.get_frame_time();
-        box_position.1 -= (window.get_mouse_wheel_move() as f32 * BOX_SPEED * delta) as i32;
+        position.1 -= (window.get_mouse_wheel_move() as f32 * BOX_SPEED * delta) as i32;
         // Draw
         window.draw(|canvas| {
             canvas.clear_background(Color::RAYWHITE);
-            canvas.draw_rectangle(
-                box_position.0,
-                box_position.1,
-                BOX_SIZE,
-                BOX_SIZE,
-                Color::MAROON,
-            );
+            canvas.draw_rectangle(position.0, position.1, BOX_SIZE, BOX_SIZE, Color::MAROON);
             canvas.draw_text(
                 "Use mouse wheel to move the cube up and down!",
                 10,
@@ -35,7 +29,7 @@ fn main() -> Result {
                 Color::GRAY,
             );
             canvas.draw_text(
-                &format!("Box position Y: {:03}", box_position.1),
+                &format!("Box position Y: {:03}", position.1),
                 10,
                 40,
                 20,
