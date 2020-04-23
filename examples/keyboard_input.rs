7f3290410334eff9f356319e975c9497fdfc6a3d
Mickaël Malécot <mickael.malecot@gmail.com>
2020-04-23 13:02:32 +0200

    Improve keyboard input example

--- examples/keyboard_input.rs
+++ examples/keyboard_input.rs
@@ -11,7 +11,7 @@ fn main() -> Result {
         .vsync()
         .msaa_4x()
         .build()?;
-    let mut ball_position = Vector2::new(
+    let mut position = Vector2::new(
         window.get_screen_width() as f32 / 2.0,
         window.get_screen_height() as f32 / 2.0,
     );
@@ -19,21 +19,21 @@ fn main() -> Result {
         // Update
         let delta = window.get_frame_time();
         if window.is_key_down(Key::Right) {
-            ball_position.x += BALL_SPEED * delta;
+            position.x += BALL_SPEED * delta;
         }
         if window.is_key_down(Key::Left) {
-            ball_position.x -= BALL_SPEED * delta;
+            position.x -= BALL_SPEED * delta;
         }
         if window.is_key_down(Key::Up) {
-            ball_position.y -= BALL_SPEED * delta;
+            position.y -= BALL_SPEED * delta;
         }
         if window.is_key_down(Key::Down) {
-            ball_position.y += BALL_SPEED * delta;
+            position.y += BALL_SPEED * delta;
         }
         // Draw
         window.draw(|canvas| {
             canvas.clear_background(Color::RAYWHITE);
-            canvas.draw_circle_vec(ball_position, 50.0, Color::MAROON);
+            canvas.draw_circle_vec(position, 50.0, Color::MAROON);
             canvas.draw_text("Move the ball with arrow keys", 10, 10, 20, Color::DARKGRAY);
         });
     }
