0d50d749e2fe4845a91280d3fe0ef48d01ea116f
Mickaël Malécot <mickael.malecot@gmail.com>
2020-04-23 17:28:50 +0200

    Create a generic error type

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,33 @@
 //! Error module.
 
-use std::{
-    error::Error,
-    fmt::{Display, Formatter, Result},
-};
+use std::fmt::{Display, Formatter, Result};
+
+/// Generic error type.
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum Error {
+    CreateWindowError(CreateWindowError),
+    LoadTextureError(LoadTextureError),
+}
+
+impl Display for Error {
+    fn fmt(&self, formatter: &mut Formatter) -> Result {
+        write!(formatter, "{:?}", self)
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl From<CreateWindowError> for Error {
+    fn from(error: CreateWindowError) -> Error {
+        Error::CreateWindowError(error)
+    }
+}
+
+impl From<LoadTextureError> for Error {
+    fn from(error: LoadTextureError) -> Error {
+        Error::LoadTextureError(error)
+    }
+}
 
 /// Kinds of window creation errors.
 #[derive(Clone, Debug, Eq, PartialEq)]
@@ -18,7 +42,7 @@ impl Display for CreateWindowError {
     }
 }
 
-impl Error for CreateWindowError {}
+impl std::error::Error for CreateWindowError {}
 
 /// Kinds of texture loading errors.
 #[derive(Clone, Debug, Eq, PartialEq)]
@@ -32,4 +56,4 @@ impl Display for LoadTextureError {
     }
 }
 
-impl Error for LoadTextureError {}
+impl std::error::Error for LoadTextureError {}
--- src/result.rs
+++ src/result.rs
@@ -1,4 +1,4 @@
 //! Result module.
 
 /// Convenient result type consisting of a return type and an error.
-pub type Result<T = (), E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
+pub type Result<T = (), E = crate::error::Error> = std::result::Result<T, E>;
