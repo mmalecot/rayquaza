cb50f019d0e79b349939d5a58dff481adc7ccdae
Mickaël Malécot <mickael.malecot@gmail.com>
2020-04-25 23:51:35 +0200

    Embed raylib

--- build.rs
+++ build.rs
@@ -1,80 +1,30 @@
 use cmake::Config;
-use flate2::read::GzDecoder;
-use std::{
-    env,
-    error::Error,
-    fs::{self, File},
-    io,
-    path::Path,
-};
-use tar::Archive;
-
-const GITHUB_DOWNLOAD_URL: &str = "https://codeload.github.com";
-const RAYLIB_REPOSITORY_OWNER: &str = "mmalecot";
-const RAYLIB_REPOSITORY_NAME: &str = "raylib";
-const RAYLIB_ARCHIVE_EXTENSION: &str = "tar.gz";
-const RAYLIB_VERSION: &str = "3.0.0";
+use fs_extra::{dir, dir::CopyOptions};
+use std::{env, error::Error, fs, path::Path};
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Computes paths
-    let out_directory = env::var("OUT_DIR")?;
-    let out_directory = Path::new(&out_directory);
-    let raylib_archive_url = format!(
-        "{url}/{owner}/{repository}/{extension}/{reference}",
-        url = GITHUB_DOWNLOAD_URL,
-        owner = RAYLIB_REPOSITORY_OWNER,
-        repository = RAYLIB_REPOSITORY_NAME,
-        extension = RAYLIB_ARCHIVE_EXTENSION,
-        reference = RAYLIB_VERSION,
-    );
-    let raylib_archive_file = format!(
-        "{name}-{version}.{extension}",
-        name = RAYLIB_REPOSITORY_NAME,
-        version = RAYLIB_VERSION,
-        extension = RAYLIB_ARCHIVE_EXTENSION
-    );
-    let raylib_archive_file = out_directory.join(&raylib_archive_file);
-    let raylib_directory = format!(
-        "{name}-{version}",
-        name = RAYLIB_REPOSITORY_NAME,
-        version = RAYLIB_VERSION,
-    );
-    let raylib_directory = out_directory.join(&raylib_directory);
+    let source_directory = env::var("CARGO_MANIFEST_DIR")?;
+    let source_directory = Path::new(&source_directory);
+    let destination_directory = env::var("OUT_DIR")?;
+    let destination_directory = Path::new(&destination_directory);
+    let raylib_source_directory = source_directory.join("external/raylib");
+    let raylib_destination_directory = destination_directory.join("raylib");
 
-    if !raylib_directory.exists() {
-        // Downloads raylib
-        let response = ureq::get(&raylib_archive_url).call();
-        let mut reader = response.into_reader();
-        let mut writer = File::create(&raylib_archive_file)?;
-        io::copy(&mut reader, &mut writer)?;
-
-        // Decompresses archive
-        let file = File::open(&raylib_archive_file)?;
-        let reader = GzDecoder::new(file);
-        let mut archive = Archive::new(reader);
-        archive.unpack(&out_directory)?;
-        fs::remove_file(&raylib_archive_file)?;
-    }
+    // Copies raylib to output directory.
+    let options = CopyOptions::new();
+    dir::copy(&raylib_source_directory, &destination_directory, &options)?;
 
     // Compiles raylib
-    let mut config = Config::new(&raylib_directory);
+    let mut config = Config::new(&raylib_destination_directory);
     let build_directory = config
         .define("CMAKE_INSTALL_LIBDIR", "lib")
-        .define("BUILD_EXAMPLES", "OFF")
-        .define("BUILD_GAMES", "OFF")
         .define("SUPPORT_CAMERA_SYSTEM", "OFF")
         .define("SUPPORT_GESTURES_SYSTEM", "OFF")
-        .define("SUPPORT_MOUSE_GESTURES", "OFF")
-        .define("SUPPORT_SSH_KEYBOARD_RPI", "OFF")
-        .define("SUPPORT_MOUSE_CURSOR_RPI", "OFF")
         .define("SUPPORT_SCREEN_CAPTURE", "OFF")
         .define("SUPPORT_GIF_RECORDING", "OFF")
         .define("SUPPORT_COMPRESSION_API", "OFF")
-        .define("SUPPORT_DATA_STORAGE", "OFF")
         .define("SUPPORT_VR_SIMULATOR", "OFF")
-        .define("SUPPORT_IMAGE_EXPORT", "OFF")
-        .define("SUPPORT_IMAGE_MANIPULATION", "OFF")
-        .define("SUPPORT_IMAGE_GENERATION", "OFF")
         .define("SUPPORT_FILEFORMAT_FNT", "OFF")
         .define("SUPPORT_FILEFORMAT_TTF", "OFF")
         .define("SUPPORT_FILEFORMAT_OBJ", "OFF")
@@ -91,6 +41,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         .define("SUPPORT_TRACELOG", "OFF")
         .define("STATIC", "TRUE")
         .build();
+    fs::remove_dir_all(&raylib_destination_directory)?;
     let library_directory = build_directory.join("lib");
 
     // Links libraries
---
+++ external/raylib/README.md
@@ -0,0 +1,7 @@
+# raylib
+
+Rayquaza-specific fork of [raylib](https://www.raylib.com/).
+
+## License
+
+raylib is licensed under an unmodified zlib/libpng license, which is an OSI-certified, BSD-like license that allows static linking with closed source software. Check [LICENSE](LICENSE.md) for further details.
