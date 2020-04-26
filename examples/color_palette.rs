c03629ec27e9edc278085f1cfec76de8c506b3a5
Mickaël Malécot <mickael.malecot@gmail.com>
2020-04-26 15:29:15 +0200

    Improve color palette example

--- examples/color_palette.rs
+++ examples/color_palette.rs
@@ -9,89 +9,191 @@ enum State {
     Hover,
 }
 
+struct Item<'s> {
+    color: Color,
+    title: &'s str,
+    state: State,
+    rectangle: Rectangle,
+}
+
 fn main() -> Result {
     let window = WindowBuilder::new()
         .title("Color palette")
         .vsync()
         .build()?;
-    let mut colors = vec![
-        (Color::DARKGRAY, "DARKGRAY", State::Normal),
-        (Color::MAROON, "MAROON", State::Normal),
-        (Color::ORANGE, "ORANGE", State::Normal),
-        (Color::DARKGREEN, "DARKGREEN", State::Normal),
-        (Color::DARKBLUE, "DARKBLUE", State::Normal),
-        (Color::DARKPURPLE, "DARKPURPLE", State::Normal),
-        (Color::DARKBROWN, "DARKBROWN", State::Normal),
-        (Color::GRAY, "GRAY", State::Normal),
-        (Color::RED, "RED", State::Normal),
-        (Color::GOLD, "GOLD", State::Normal),
-        (Color::LIME, "LIME", State::Normal),
-        (Color::BLUE, "BLUE", State::Normal),
-        (Color::VIOLET, "VIOLET", State::Normal),
-        (Color::BROWN, "BROWN", State::Normal),
-        (Color::LIGHTGRAY, "LIGHTGRAY", State::Normal),
-        (Color::PINK, "PINK", State::Normal),
-        (Color::YELLOW, "YELLOW", State::Normal),
-        (Color::GREEN, "GREEN", State::Normal),
-        (Color::SKYBLUE, "SKYBLUE", State::Normal),
-        (Color::PURPLE, "PURPLE", State::Normal),
-        (Color::BEIGE, "BEIGE", State::Normal),
+    let mut items = vec![
+        Item {
+            color: Color::DARKGRAY,
+            title: "DARKGRAY",
+            state: State::Normal,
+            rectangle: Rectangle::default(),
+        },
+        Item {
+            color: Color::MAROON,
+            title: "MAROON",
+            state: State::Normal,
+            rectangle: Rectangle::default(),
+        },
+        Item {
+            color: Color::ORANGE,
+            title: "ORANGE",
+            state: State::Normal,
+            rectangle: Rectangle::default(),
+        },
+        Item {
+            color: Color::DARKGREEN,
+            title: "DARKGREEN",
+            state: State::Normal,
+            rectangle: Rectangle::default(),
+        },
+        Item {
+            color: Color::DARKBLUE,
+            title: "DARKBLUE",
+            state: State::Normal,
+            rectangle: Rectangle::default(),
+        },
+        Item {
+            color: Color::DARKPURPLE,
+            title: "DARKPURPLE",
+            state: State::Normal,
+            rectangle: Rectangle::default(),
+        },
+        Item {
+            color: Color::DARKBROWN,
+            title: "DARKBROWN",
+            state: State::Normal,
+            rectangle: Rectangle::default(),
+        },
+        Item {
+            color: Color::GRAY,
+            title: "GRAY",
+            state: State::Normal,
+            rectangle: Rectangle::default(),
+        },
+        Item {
+            color: Color::RED,
+            title: "RED",
+            state: State::Normal,
+            rectangle: Rectangle::default(),
+        },
+        Item {
+            color: Color::GOLD,
+            title: "GOLD",
+            state: State::Normal,
+            rectangle: Rectangle::default(),
+        },
+        Item {
+            color: Color::LIME,
+            title: "LIME",
+            state: State::Normal,
+            rectangle: Rectangle::default(),
+        },
+        Item {
+            color: Color::BLUE,
+            title: "BLUE",
+            state: State::Normal,
+            rectangle: Rectangle::default(),
+        },
+        Item {
+            color: Color::VIOLET,
+            title: "VIOLET",
+            state: State::Normal,
+            rectangle: Rectangle::default(),
+        },
+        Item {
+            color: Color::BROWN,
+            title: "BROWN",
+            state: State::Normal,
+            rectangle: Rectangle::default(),
+        },
+        Item {
+            color: Color::LIGHTGRAY,
+            title: "LIGHTGRAY",
+            state: State::Normal,
+            rectangle: Rectangle::default(),
+        },
+        Item {
+            color: Color::PINK,
+            title: "PINK",
+            state: State::Normal,
+            rectangle: Rectangle::default(),
+        },
+        Item {
+            color: Color::YELLOW,
+            title: "YELLOW",
+            state: State::Normal,
+            rectangle: Rectangle::default(),
+        },
+        Item {
+            color: Color::GREEN,
+            title: "GREEN",
+            state: State::Normal,
+            rectangle: Rectangle::default(),
+        },
+        Item {
+            color: Color::SKYBLUE,
+            title: "SKYBLUE",
+            state: State::Normal,
+            rectangle: Rectangle::default(),
+        },
+        Item {
+            color: Color::PURPLE,
+            title: "PURPLE",
+            state: State::Normal,
+            rectangle: Rectangle::default(),
+        },
+        Item {
+            color: Color::BEIGE,
+            title: "BEIGE",
+            state: State::Normal,
+            rectangle: Rectangle::default(),
+        },
     ];
-    let rectangles: Vec<_> = colors
-        .iter()
-        .enumerate()
-        .map(|(index, _)| {
-            Rectangle::new(
-                20.0 + 100.0 * (index % 7) as f32 + 10.0 * (index % 7) as f32,
-                80.0 + 100.0 * (index / 7) as f32 + 10.0 * (index / 7) as f32,
-                100.0,
-                100.0,
-            )
-        })
-        .collect();
+    items.iter_mut().enumerate().for_each(|(index, item)| {
+        item.rectangle.x = 20.0 + 100.0 * (index % 7) as f32 + 10.0 * (index % 7) as f32;
+        item.rectangle.y = 80.0 + 100.0 * (index / 7) as f32 + 10.0 * (index / 7) as f32;
+        item.rectangle.width = 100.0;
+        item.rectangle.height = 100.0;
+    });
     while !window.should_close() {
         // Updates
-        for index in 0..colors.len() {
-            colors[index].2 =
-                if check_point_rectangle(window.get_mouse_position(), rectangles[index]) {
-                    State::Hover
-                } else {
-                    State::Normal
-                }
-        }
+        items.iter_mut().for_each(|item| {
+            item.state = if check_point_rectangle(window.get_mouse_position(), item.rectangle) {
+                State::Hover
+            } else {
+                State::Normal
+            }
+        });
         // Draws
         window.draw(|canvas| {
             canvas.clear_background(Color::RAYWHITE);
             canvas.draw_text("Color palette", 20, 30, 20, Color::BLACK);
-            for index in 0..colors.len() {
+            items.iter_mut().for_each(|item| {
                 canvas.draw_rectangle_rec(
-                    rectangles[index],
-                    colors[index].0.fade(if colors[index].2 == State::Hover {
-                        0.6
-                    } else {
-                        1.0
-                    }),
+                    item.rectangle,
+                    item.color
+                        .fade(if item.state == State::Hover { 0.6 } else { 1.0 }),
                 );
-                if window.is_key_down(Key::Space) || colors[index].2 == State::Hover {
+                if window.is_key_down(Key::Space) || item.state == State::Hover {
                     canvas.draw_rectangle(
-                        rectangles[index].x as i32,
-                        rectangles[index].y as i32 + rectangles[index].height as i32 - 26,
-                        rectangles[index].width as i32,
+                        item.rectangle.x as i32,
+                        item.rectangle.y as i32 + item.rectangle.height as i32 - 26,
+                        item.rectangle.width as i32,
                         20,
                         Color::BLACK,
                     );
-                    canvas.draw_rectangle_lines_ex(rectangles[index], 6, Color::BLACK.fade(0.3));
+                    canvas.draw_rectangle_lines_ex(item.rectangle, 6, Color::BLACK.fade(0.3));
                     canvas.draw_text(
-                        colors[index].1,
-                        rectangles[index].x as i32 + rectangles[index].width as i32
-                            - measure_text(colors[index].1, 10)
+                        item.title,
+                        item.rectangle.x as i32 + item.rectangle.width as i32
+                            - measure_text(item.title, 10)
                             - 12,
-                        rectangles[index].y as i32 + rectangles[index].height as i32 - 20,
+                        item.rectangle.y as i32 + item.rectangle.height as i32 - 20,
                         10,
-                        colors[index].0,
+                        item.color,
                     );
                 }
-            }
+            });
             canvas.draw_text(
                 "Press SPACE to see all colors",
                 window.get_width() - 180,
