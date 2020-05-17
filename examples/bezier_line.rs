879b5cadd643d749f4d16db089c844b2402a4ae1
Mickaël Malécot <mickael.malecot@gmail.com>
2020-05-18 00:41:06 +0200

    Add bezier line example

---
+++ examples/bezier_line.rs
@@ -0,0 +1,39 @@
+use rayquaza::{
+    color::Color, input::MouseButton, math::Vector2, result::Result, window::WindowBuilder,
+};
+
+fn main() -> Result {
+    let window = WindowBuilder::new()
+        .title("Bezier line")
+        .resizable()
+        .vsync()
+        .msaa_4x()
+        .build()?;
+    let mut start = Vector2::new(100.0, 100.0);
+    let mut end = Vector2::new(
+        window.get_width() as f32 - 100.0,
+        window.get_height() as f32 - 100.0,
+    );
+    while !window.should_close() {
+        // Updates
+        if window.is_mouse_button_down(MouseButton::Left) {
+            start = window.get_mouse_position();
+        }
+        if window.is_mouse_button_down(MouseButton::Right) {
+            end = window.get_mouse_position();
+        }
+        // Draws
+        window.draw(|canvas| {
+            canvas.clear_background(Color::RAYWHITE);
+            canvas.draw_text(
+                "Use mouse click to define line start and end points",
+                20,
+                20,
+                20,
+                Color::GRAY,
+            );
+            canvas.draw_line_bezier(start, end, 2.0, Color::RED);
+        });
+    }
+    Ok(())
+}
